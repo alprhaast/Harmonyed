fe3b5de09c578e6d1f7f86f9ee86988212b23647
Rhaast <Rhaastdeveloper@gmail.com>
2025-01-24 12:52:15 -0600

    Homepage UI

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,21 +1,11 @@
 export default function Footer() {
     return (
         <footer className="flex items-center justify-center w-full h-24 border-t">
-            <a
-                className="flex items-center justify-center"
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Powered by{" "}
-                <img
-                    src="/vercel.svg"
-                    alt="Vercel Logo"
-                    className="h-4 ml-2"
-                    width={92}
-                    height={16}
-                />
-            </a>
+            <div className="flex items-center justify-center space-x-2">
+                <p className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                    © 2025 Harmony Education Services
+                </p>
+            </div>
         </footer>
     );
 }
--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,16 +1,19 @@
 export default function Hero() {
-    return (
-        <section className="w-full bg-white">
-            <div className="container ml-auto mr-auto">
-                <div className="text-center py-40">
-                    <h1 className="text-5xl font-bold text-black pb-6">Harmony Education Services</h1>
-                    <p className="text-black text-2xl">Creating Harmony through unique, innovative and personalized educational solutions.</p>
-                    <div className="flex justify-center">
-                        <a href="/login" className="btn-primary">Get Started</a>
-                        <a href="/about">Learn More</a>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+	return (
+		<section className="bg-white py-20">
+			<div className="py-8 px-4 mx-auto max-w-screen-xl text-center">
+				<h1 className="mb-5 text-3xl font-extrabold md:text-4xl lg:text-5xl text-black">
+					Harmony Education Services
+				</h1>
+				<p className="mb-8 text-lg font-normal text-gray-900 lg:text-xl sm:px-16 xl:px-48">
+					Creating Harmony through unique, innovative and personalized educational solutions.
+				</p>
+				<div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+					<a href="/login" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-black rounded-lg border border-gray-300 hover:bg-gray-800 hover:border-gray-800 hover:text-white focus:ring-4 focus:ring-gray-100">
+						Get Started
+					</a>
+				</div>
+			</div>
+		</section>
+	);
 }
